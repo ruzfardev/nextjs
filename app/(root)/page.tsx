fd138eb16bbbccc3a34ed8f3d0faa91c7ba17e3d
Farrukh Ruzmetov <farrukhruzmetov2002@gmail.com>
2025-04-09 00:41:28 +0500

    Add Search component and integrate it into the Home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,5 @@
+import { Search } from "@/app/components";
+
 export default function Home() {
   return (
     <section className="pink_container">
@@ -7,6 +9,7 @@ export default function Home() {
       <p className="sub-heading !max-w-3xl">
         Submit ideas, vote on Pitches, and Get Noticed in Virtual Competitions.
       </p>
+      <Search />
     </section>
   );
 }
---
+++ app/components/search/Search.tsx
@@ -0,0 +1,3 @@
+export const Search = () => {
+  return <div className="flex-center mx-auto mt-10 max-w-3xl">Search</div>;
+};
